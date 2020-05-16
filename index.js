8fe0443759391b4ad66a5328f78f258aa7e29d21
Carl521 <63449271+Carl521@users.noreply.github.com>
2020-05-16 14:31:35 +0300

    add file for exercise 1a

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-const Button = ({ onClick, namn }) => (  <button onClick={onClick}>    {namn}  </button>)
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Statistics = ({text, value}) => {
+
+const Part = (props) =>{
   return(
-    <p> {text}: {value} </p>
-  )
-  }
+    <div>     
+     <p> {props.part} {props.number_of_ex} </p>    
+     </div>
+   
+     )
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
-  const average = (good*1 +  bad*(-1)) / (good + neutral + bad)
-  const positive = ((good) / (good + neutral + bad))*100
-  
-  if (good + neutral + bad > 0  ){
-   return (
-    <div>
-      
-      <h1> Give feedback </h1>
-
-      <Button onClick={handleGoodClick} namn = 'good'/>
-      <Button onClick={handleNeutralClick} namn= 'neutral'/>
-      <Button onClick={handleBadClick} namn= 'bad'/>
-      
-      <h1> Statistics </h1>
-      
-      <Statistics text = 'good' value = {good}/>
-      <Statistics text = 'neutral' value = {neutral}/>
-      <Statistics text = 'bad' value = {bad}/>
-      <Statistics text = 'average' value = {average}/>
-      <Statistics text = 'positive' value = {positive} />
-    
-    </div>
-  )}
-  
-  else{
-   return (
-    <div>
-      <h1> Give feedback </h1>
-
-      <Button onClick={handleGoodClick} namn = 'good'/>
-      <Button onClick={handleNeutralClick} namn= 'neutral'/>
-      <Button onClick={handleBadClick} namn= 'bad'/>
-      
-      <h1> Statistics </h1>
-
-      <p> no feedback given </p>
-    
-    </div>
-  )}
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+const Header = (props) =>{
+  return(
+         
+     <p> {props.course_name} </p>    
+     
+
+  )}
+const Content = (props) =>{
+  const name_of_part1 = props.part_name1
+  const exercises_number1 = props.number_of_exercises1
+  const name_of_part2 = props.part_name2
+  const exercises_number2 = props.number_of_exercises2
+  const name_of_part3 = props.part_name3
+  const exercises_number3 = props.number_of_exercises3
+  return(
+   <div>     
+    <Part part = {name_of_part1} number_of_ex = {exercises_number1} />
+    <Part part = {name_of_part2} number_of_ex = {exercises_number2} />
+    <Part part = {name_of_part3} number_of_ex = {exercises_number3} />
+    </div>
+  
+    )}
+const Total = (props) =>{
+  return(
+   <div>     
+    <p> Number of exercises {props.tot_number_of_exercises} </p>    
+    </div>
+      
+        )}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course_name={course} />
+      
+      <Content part_name1 = {part1} number_of_exercises1 = {exercises1} 
+      part_name2 = {part2} number_of_exercises2 = {exercises2} 
+      part_name3 = {part3} number_of_exercises3 = {exercises3}/>
+      
+      <Total tot_number_of_exercises =  {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
